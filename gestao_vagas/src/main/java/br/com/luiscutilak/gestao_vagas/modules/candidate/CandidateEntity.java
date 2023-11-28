b38dd0bb391ebff41bc674d8a703f15438d9546a
Luis Fernando Cutilak <luiscutilak@gmail.com>
2023-11-28 12:08:12 -0300

    feat: adicionando mensagem de tratamento de erro no campo PASSWORD.

--- gestao_vagas/src/main/java/br/com/luiscutilak/gestao_vagas/modules/candidate/CandidateEntity.java
+++ gestao_vagas/src/main/java/br/com/luiscutilak/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -20,7 +20,7 @@ public class CandidateEntity {
     @Email(message = "O campo [email] deve conter um email válido.")
     private String email;
 
-    @Length(min = 10, max = 100)
+    @Length(min = 10, max = 100, message = "O campo [senha] deve conter entre (10) e (100) caracteres")
     private String password;
     private String description;
     private String curriculum;
