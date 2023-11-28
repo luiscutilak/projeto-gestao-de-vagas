77f19c1dffc7fbcf27383a48b4d7e130e952369d
Luis Fernando Cutilak <luiscutilak@gmail.com>
2023-11-28 15:00:35 -0300

    feat: adicionando camada Banco de dados>Interface. Uso do Autowired

--- gestao_vagas/src/main/java/br/com/luiscutilak/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ gestao_vagas/src/main/java/br/com/luiscutilak/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,22 +1,26 @@
 package br.com.luiscutilak.gestao_vagas.modules.candidate.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.luiscutilak.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.luiscutilak.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
     
 
     @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {  //criando candidato. Buscando atributos da classe Candidate Entity
-        System.out.println("Candidato");
-        System.out.println(candidateEntity.getName());
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {  //criando candidato. Buscando atributos da classe Candidate Entity
+        return this.candidateRepository.save(candidateEntity);
     }
 
 }
